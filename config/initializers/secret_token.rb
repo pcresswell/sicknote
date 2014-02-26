d70abc83bbb131ad394a99c93d1f9f39043e806a
Peter Cresswell <pcresswell@gmail.com>
2014-02-26 14:44:05 -0500

    new rails app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sicknote::Application.config.secret_key_base = '2d7e0ec44af654fdb1f2199024a0e25902a4f4865971b370ab29bf891f11b6770cde06e153a3469cca3161bebcd91b71bf6d3887f2eb1a5be776806cc3064908'
